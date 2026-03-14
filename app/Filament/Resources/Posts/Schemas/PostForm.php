dd028da7d65cb46535c9cf917b5ebc537c465d84
dong <2453611300@qq.com>
2026-03-14 17:34:19 +0800

    修改宽度问题

--- app/Filament/Resources/Posts/Schemas/PostForm.php
+++ app/Filament/Resources/Posts/Schemas/PostForm.php
@@ -21,6 +21,7 @@ class PostForm
     public static function configure(Schema $schema): Schema
     {
         return $schema
+            ->columns(1)
             ->components([
                 Section::make()
                     ->schema([
